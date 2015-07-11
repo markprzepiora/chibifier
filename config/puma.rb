f438c17e91d8a588fae303fea2ac6c0644521d1b
Mark Przepiora <mark.przepiora@gmail.com>
2015-07-10 22:21:55 -0600

    Do not preload app in puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,6 @@
 workers Integer(ENV['PUMA_WORKERS'] || 2)
 threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 32)
 
-preload_app!
-
 rackup      DefaultRackup
 port        ENV['PORT']     || 9293
 environment ENV['RACK_ENV'] || 'development'
